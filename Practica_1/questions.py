53775afbed363a31dfc11ecfe0c48ca3bd2416a1
Valentin Fiaccola <digitalentinty@gmail.com>
2026-03-16 22:18:00 -0300

    feat: agregar puntaje al juego

--- Practica_1/questions.py
+++ Practica_1/questions.py
@@ -12,6 +12,7 @@ words = [
 word = random.choice(words)
 guessed = []
 attempts = 6
+point = 0
 print("¡Bienvenido al Ahorcado!")
 print()
 while attempts > 0:
@@ -26,6 +27,7 @@ while attempts > 0:
     # Verificar si el jugador ya adivinó la palabra completa
     if "_" not in progress:
         print("¡Ganaste!")
+        point +=6
         break
 
     print(f"Intentos restantes: {attempts}")
@@ -45,7 +47,10 @@ while attempts > 0:
     else:
         guessed.append(letter)
         attempts -= 1
+        point -= 1
         print("Esa letra no está en la palabra.")
     print()
 else:
     print(f"¡Perdiste! La palabra era: {word}")
+    point=0
+print (f"Tu puntaje es: {point}")
